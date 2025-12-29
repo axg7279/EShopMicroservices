46359aad41dde0c9cde865c899d3de96bc4fb535
axg axg <axg7279@gmail.com>
2025-12-28 20:36:04 -0500

    Adding packages including Mapster, Carter. Added Product Post minimal API

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,30 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+using MediatR;
+
+namespace Catalog.API.Products.CreateProduct
+{
+    public record CreateProductCommand(
+        string Name,
+        List<string> Category,
+        string Description,
+        string ImageFile,
+        decimal Price) : ICommand<CreateProductResult>;
+
+    public record CreateProductResult(Guid Id);
+    public class CreateProductCommandHandler 
+        : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            var product = new Product { 
+                Name = request.Name,
+                Category = request.Category,
+                Description = request.Description,
+                ImageFile = request.ImageFile,
+                Price = request.Price
+            };
+            return new CreateProductResult(Guid.NewGuid());
+        }
+    }
+}
